3fb65fad533c7c290c1cdae08471c002d2497a71
Jonny Johansson <jonny.k.johansson@gmail.com>
2018-04-21 20:07:47 +0200

    Bugfix for when config is not loaded yet or is invalid

--- RaidPlannerBot/Extensions.cs
+++ RaidPlannerBot/Extensions.cs
@@ -6,7 +6,9 @@ namespace RaidPlannerBot
     {
         public static T Log<T>(this T stuff, bool onlyIfDebug = false)
         {
-            if ((AppConfig.Shared.Debug && onlyIfDebug) || !onlyIfDebug)
+            bool allowDebugOutput = AppConfig.Shared?.Debug ?? true;
+
+            if ((allowDebugOutput && onlyIfDebug) || !onlyIfDebug)
             {
                 var timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
                 Console.Out.WriteLine($"[{timestamp}] {stuff}");
